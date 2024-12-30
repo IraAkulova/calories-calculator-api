beaeeb2bafc7e940014a25f57e581a570c03ce5c
Ira <ira.akulova2019@gmail.com>
2024-12-30 11:26:31 +0200

    configure main file

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,10 +4,10 @@ import { ConfigModule } from '@nestjs/config';
 import { AuthModule } from './auth/auth.module';
 import { UserModule } from './user/user.module';
 import { AnalyticsModule } from './analytics/analytics.module';
-import { DiaryModule } from './diary/diary.module';
 import { ProductModule } from './product/product.module';
 import { PrismaModule } from './prisma/prisma.module';
 import { JwtStrategy } from './auth/strategy/jwt.strategy';
+import { DiaryRecordModule } from './diary-record/diary-record.module';
 
 @Module({
   imports: [
@@ -17,9 +17,9 @@ import { JwtStrategy } from './auth/strategy/jwt.strategy';
     AuthModule,
     UserModule,
     AnalyticsModule,
-    DiaryModule,
     ProductModule,
     PrismaModule,
+    DiaryRecordModule,
   ],
   controllers: [],
   providers: [JwtStrategy],
--- src/main.ts
+++ src/main.ts
@@ -1,10 +1,31 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
 import { ValidationPipe } from '@nestjs/common';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.useGlobalPipes(new ValidationPipe());
+
+  const config = new DocumentBuilder()
+    .setTitle('Calorie Calculator')
+    .setDescription(
+      '<p>An API for managing user accounts, tracking calorie intake, and searching product nutritional information. <br/>It enables users to register, log in, and maintain a personalized food diary with calorie tracking. <br/>This API is designed for health - conscious individuals looking to monitor their dietary habits effectively.</p> ',
+    )
+    .setVersion('1.0')
+    .addTag('Calorie Calculator API')
+    .addBearerAuth(
+      {
+        type: 'http',
+        scheme: 'bearer',
+        bearerFormat: 'JWT',
+      },
+      'access-token',
+    )
+    .build();
+  const document = SwaggerModule.createDocument(app, config);
+  SwaggerModule.setup('api', app, document);
+
   await app.listen(3334);
 }
 bootstrap();
